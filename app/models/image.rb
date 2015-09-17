287736f0995dc67ac5dfc99ba59b995b1c41d857
zalbo <sbabbari@gmail.com>
2015-09-17 10:30:43 +0200

    image in articles

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
   belongs_to :article
-  has_attached_file :upload_photo, styles: { thumb_medium: "291x291#", thumb_small: "100x100#" }, default_url: "/images/:style/missing.png"
+  has_attached_file :upload_photo, styles: {medium: "720x720#", thumb_medium: "291x291#", thumb_small: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :upload_photo, content_type: /\Aimage\/.*\Z/
 end

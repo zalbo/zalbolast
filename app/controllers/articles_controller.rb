f2120659a6c8e5e7a0469e20f2510a09233e8c0e
zalbo <sbabbari@gmail.com>
2015-09-17 15:58:54 +0200

    add authenticate

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :authenticate_user!, only: [:new , :update, :destroy ]
   before_action :set_article, only: [:show, :edit, :update, :destroy ]
 
 
